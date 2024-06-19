32a1cbb84704f14c9d633fef6f64f5460b2b93ca
Mirae Kwak <alfo9490@naver.com>
2024-06-20 00:24:28 +0900

    :tada: findog 프로젝트 시작

---
+++ src/test/java/com/amatta/findog/FindogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amatta.findog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
